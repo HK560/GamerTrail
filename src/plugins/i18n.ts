61b519d730265cf5da0889c18291e9e1c63b145c
HK560 <lan2496846801@gmail.com>
2024-10-15 22:32:05 +0800

    init: add eslint and prettier

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,20 +1,19 @@
-import { createI18n } from 'vue-i18n'
-import zhCN from '../locales/zh-CN.json'
-import en from '../locales/en.json'
+import { createI18n } from "vue-i18n";
+import zhCN from "../locales/zh-CN.json";
+import en from "../locales/en.json";
 export const i18n = createI18n({
-    legacy: false, // you must specify 'legacy: false' option
-    globalInjection: true,
-    locale: 'zh', // set the initial locale
-    fallbackLocale: 'zh', // set the fallback locale
-    messages: {
-      en: {
-        // your english translations here
-        ...en
-      },
-      zh: {
-        // your chinese translations here
-        ...zhCN
-      }
+  legacy: false, // you must specify 'legacy: false' option
+  globalInjection: true,
+  locale: "zh", // set the initial locale
+  fallbackLocale: "zh", // set the fallback locale
+  messages: {
+    en: {
+      // your english translations here
+      ...en
+    },
+    zh: {
+      // your chinese translations here
+      ...zhCN
     }
-  })
-  
+  }
+});
